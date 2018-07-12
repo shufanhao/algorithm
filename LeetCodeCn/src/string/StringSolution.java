645a2d9bca5dc7fce96633ce47c1f1c2749d5e56
haofan <fanhaoisfrank@gamil.com>
2018-07-12 23:10:30 +0800

    add: question 2

--- LeetCodeCn/src/string/StringSolution.java
+++ LeetCodeCn/src/string/StringSolution.java
@@ -36,8 +36,31 @@ public class StringSolution {
         }
         return new String((sChar));
     }
+
+    /**
+     * 给定一个32位有符号整数，将整数中的数字进行反转
+     * 思路：根据 x % 10 = 个位数，x/10 = 除了个位数的前面几个数，还要考虑是100的这种情况
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int tail = x % 10; //取尾数
+            int newResult = result * 10 + tail;
+            //下面这行code 是为了cover case: 1000
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x /10;
+        }
+        return result;
+
+    }
     public static void main(String args[]) {
         StringSolution ss = new StringSolution();
         System.out.println("题目1： " + ss.reverseString("haofan"));
+
+        System.out.println("题目2： " + ss.reverse(100));
     }
 }
