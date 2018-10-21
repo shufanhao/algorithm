1033996b4e6487131f38a9937cacdb29590c440d
haofan <fanhaoisfrank@gamil.com>
2018-10-21 19:19:27 +0800

    second to write

--- sort/src/MergeSort.java
+++ sort/src/MergeSort.java
@@ -3,45 +3,64 @@ import java.util.Arrays;
 
 //https://www.cnblogs.com/chengxiao/p/6194356.html
 public class MergeSort {
-
+    /**
+     * 归并排序：时间复杂度NlogN,
+     * 采用 分治策略
+     * 注意整个逻辑：
+     * 分：就是递归去分。
+     * 治：就是merge 两个有序元素
+     * @param args
+     */
     public static void main(String args[]) {
         int [] arr = {9,8,7,6,5,4,3,2,1};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int arr[]) {
-        int[] temp = new int[arr.length];
+    public static void sort(int[] arr) {
+        // 创建temp
+        int [] temp = new int[arr.length];
         sort(arr, 0, arr.length -1, temp);
     }
-    public static void sort(int arr[], int left, int right, int temp[]) {
+
+    public static void sort(int[] arr, int left, int right, int[] temp) {
+        // 错误1： 这个写成了left <= right
         if (left < right) {
-            int mid = (left + right) /2;
+            // 分，
+            int mid = (left + right) / 2;
+            // 左边归并排序，使得左子序列有序
             sort(arr, left, mid, temp);
-            sort(arr, mid + 1, right, temp);
+            // 右边归并排序，使得右子序列有序
+            sort(arr,mid +1, right, temp);
+            // 治, 将两个有序子数组合并操作
             merge(arr, left, mid, right, temp);
         }
     }
-    public static void merge(int arr[], int left,int mid, int right, int temp[]) {
-        int i = left;
-        int j = mid +1;
+
+    public static void merge(int[] arr, int left, int mid , int right, int[] temp) {
+        int i= left;
+        int j = mid + 1;
         int t = 0;
-        while (i <= mid && j<=right) {
+        while (i <= mid && j <= right) {
             if (arr[i] <= arr[j]) {
-                temp[t++] = arr[i++];
+                temp[t ++] = arr[i ++];
             } else {
-                temp[t++] = arr[j++];
+                temp[t ++] = arr[j++];
             }
         }
-        while (i<=mid) {
+        // 将左边剩余元素填充进temp
+        while (i <=mid) {
             temp[t++] = arr[i++];
         }
+        // 将右边剩余元素填充进temp
         while (j<=right) {
             temp[t++] = arr[j++];
         }
+        // 错误2： 忘记写t = 0;
         t = 0;
+        // Copy temp 到 arr
         while (left <= right) {
-            arr[left ++] = temp[t++];
+            arr[left ++ ] = temp[t++];
         }
     }
 }
--- sort/src/QuickSort.java
+++ sort/src/QuickSort.java
@@ -1,35 +1,52 @@
 import java.util.Arrays;
 
 public class QuickSort {
+    /**
+     * 算法平均时间复杂度：N*logN, 最糟糕：N^2（正序或逆序）
+    大致思路还是排序算法中比较简单的：
+     http://wiki.jikexueyuan.com/project/easy-learn-algorithm/fast-sort.html
+     1. 两个哨兵，分别从指向左边，右边。基准数是第一个元素
+     2. 右边哨兵开始向左移动，如果发现比基准数小的，停下来。
+     3. 左边哨兵开始向右移动，如果发现比基准数大的，停下来。
+     4. 交换左右哨兵指向的元素，并继续向左，向右移动，直到左右哨兵碰头。
+     5. 交换基准数和左右哨兵共同指向的元素。
+     6. 基准数左边，右边分别按照上面的Step继续运算。
+     */
 
-    //http://wiki.jikexueyuan.com/project/easy-learn-algorithm/fast-sort.html
-    void quickSort(int arr[], int left, int right) {
+    void quickSort(int nums[], int left, int right) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
         if (left >= right) {
             return;
         }
-        int i,j,temp;
-        i = left;
-        j = right;
-        int privot = arr[i];
+        int i = left;
+        int j = right;
+        int privot = nums[i];
+        // 犯错的地方：写成了while(i >= j)
         while (i != j) {
-            while (j >i && arr[j] >= privot) {
-                j--;
+            while (j>i && nums[j] > privot) {
+                j --;
             }
-            while (j > i && arr[i] <= privot) {
-                i++;
+            while (j > i && nums[i] < privot) {
+                i ++;
             }
-            if (i < j) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            if (j > i) {
+                // 交换两个元素
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
         }
-        arr[left] = arr[i];
-        arr[i] = privot;
-        quickSort(arr, left, i-1);
-        quickSort(arr, i +1, right);
-    }
+        // 交换和基准位置的元素
+        int temp = nums[i];
+        nums[i] = privot;
+        nums[left] = temp;
 
+        // 犯错的地方：写成了quickSort(nums, left, i);
+        quickSort(nums, left, i-1);
+        quickSort(nums, i+1, right);
+    }
     public static void main(String args[]) {
         QuickSort obj = new QuickSort();
         int[] nums = {3,4,1,5,6};
