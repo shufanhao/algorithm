989f2177d2ce4586b9ef8730c0db44852e7e3c75
haofan <fanhaoisfrank@gamil.com>
2018-12-30 16:00:22 +0800

    add: 相交链表

--- LeetCodeCn/src_medium/list/ListSolutionMedium.java
+++ LeetCodeCn/src_medium/list/ListSolutionMedium.java
@@ -58,6 +58,54 @@ public class ListSolutionMedium {
         odd.next = evenHead;
         return head;
     }
+
+    /**
+     * 题目3：相交链表，编写一个程序，找到两个链表相交的起始节点
+     * https://blog.csdn.net/hocsoul/article/details/80151330
+     * 思路：先取到链表的长度，然后移动长度长的链表到另一个链表长度一致的位置，开始比较
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        int lenHeadA = 0;
+        int lenHeadB = 0;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        // 求链表A,B的长度
+        while (tempA != null) {
+            lenHeadA ++ ;
+            tempA = tempA.next;
+        }
+        while (tempB != null) {
+            lenHeadB ++ ;
+            tempB = tempB.next;
+        }
+        // 将headA 和 headB 移到要进行节点比较的位置, 只需移动长度较大的链表
+        while ((lenHeadA > lenHeadB) && headA != null) {
+            headA = headA.next;
+            lenHeadA --;
+        }
+        while ((lenHeadB > lenHeadA) && headB != null) {
+            headB = headB.next;
+            lenHeadB --;
+        }
+        while (headA != null) {
+            // 在leetcode 运行环境下，是可以用headA == headB 来认为两个链表是相等的
+            if (headA == headB) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String args[]) {
         ListSolutionMedium list = new ListSolutionMedium();
 
@@ -79,5 +127,11 @@ public class ListSolutionMedium {
         ListSolution common = new ListSolution();
         int[] input = {1,4,5,9};
         common.printList(list.oddEvenList(common.createList(input)), "题目2：");
+
+        int[] input1 = {0,8,4,5};
+        int[] input2 = {1,8,4,5};
+
+        common.printList(list.getIntersectionNode(common.createList(input1),
+                common.createList(input2)), "题目3：");
     }
 }
