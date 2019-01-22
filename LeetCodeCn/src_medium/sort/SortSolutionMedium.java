396933825f98539fd5079a5544f7ba4281a0f869
Frank <shufanhao@126.com>
2019-01-22 12:58:28 +0800

    Update SortSolutionMedium.java

---
+++ LeetCodeCn/src_medium/sort/SortSolutionMedium.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Ref: https://blog.csdn.net/x273591655/article/details/84001236
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // 统计元素的频次
+        Map<Integer, Integer> int2FreqMap = new HashMap<>(16);
+        for (int num : nums) {
+            int2FreqMap.put(num, int2FreqMap.getOrDefault(num, 0) + 1);
+        }
+        
+        // 桶排序
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (Integer key : int2FreqMap.keySet()) {
+            int freq = int2FreqMap.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        
+        // 逆序（频次由高到低）取出元素
+        List<Integer> ret = new ArrayList<>();
+        for (int i = nums.length; i >= 0 && ret.size() < k; --i) {
+            if (bucket[i] != null) {
+                ret.addAll(bucket[i]);
+            }
+        }
+        
+        return ret;
+    }
+}
