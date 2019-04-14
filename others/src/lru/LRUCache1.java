c37557cc7a4842e79177e27e6e3eedee1529412f
fanhao <shufanhao@126.com>
2019-04-14 22:56:07 +0800

    update LRU Cache

--- others/src/lru/LRUCache1.java
+++ others/src/lru/LRUCache1.java
@@ -2,92 +2,101 @@ package lru;
 
 import java.util.HashMap;
 
-public class LRUCache1<K, V> {
-    private final int MAX_CACHE_SIZE;
-    private Entry first;
-    private Entry last;
+public class LRUCache1 {
+    private int count;
+    private int capacity;
+    private HashMap<Integer, DlinkNode> cache = new HashMap<>();
+    private DlinkNode head, tail;
 
-    public LRUCache1(int cacheSize) {
-        MAX_CACHE_SIZE = cacheSize;
-        hashMap = new HashMap<K, Entry<K, V>>();
+    public LRUCache1(int capacity) {
+        this.count = 0;
+        this.capacity = capacity;
+
+        // 初始化头节点和尾节点
+        head = new DlinkNode();
+        head.pre = null;
+
+        tail = new DlinkNode();
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
     }
 
-    public void put(K key, V value) {
-        Entry entry = getEntry(key);
-        if (entry == null) {
-            if (hashMap.size() >= MAX_CACHE_SIZE) {
-                hashMap.remove(last.key);
-                removeLast();
+    public void moveToFirst(DlinkNode node) {
+        // Step1: 将改节点移出
+        removeNode(node);
+        // Step2: 将该节点拿到头部节点
+        addNode(node);
+    }
+
+    private void removeNode(DlinkNode node) {
+        DlinkNode pre = node.pre;
+        DlinkNode post = node.post;
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    private void addNode(DlinkNode node) {
+        node.pre = head;
+        node.post = head.post;
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    //弹出尾节点
+    private DlinkNode popTail() {
+        DlinkNode res = tail.pre;
+        removeNode(res);
+        return res;
+    }
+
+    public String get(int key) {
+        DlinkNode node = cache.get(key);
+        if (node == null) {
+            return null;
+        }
+        // 更新cache, 移动到开头
+        moveToFirst(node);
+        return node.value;
+    }
+
+    public void put(int key, String value) {
+        DlinkNode node = cache.get(key);
+        if (node == null) {
+            DlinkNode newNode = new DlinkNode();
+            newNode.key = key;
+            newNode.value = value;
+            cache.put(key, newNode);
+            // 将该节点放到开头
+            addNode(newNode);
+            ++count;
+            // 判断是否大于capacity
+            if (count > capacity) {
+                DlinkNode tail = popTail();
+                cache.remove(tail.key);
+                --count;
             }
-            entry = new Entry();
-            entry.key = key;
-        }
-        entry.value = value;
-        moveToFirst(entry);
-        hashMap.put(key, entry);
-    }
-
-    public V get(K key) {
-        Entry<K, V> entry = getEntry(key);
-        if (entry == null) return null;
-        moveToFirst(entry);
-        return entry.value;
-    }
-
-    public void remove(K key) {
-        Entry entry = getEntry(key);
-        if (entry != null) {
-            if (entry.pre != null) entry.pre.next = entry.next;
-            if (entry.next != null) entry.next.pre = entry.pre;
-            if (entry == first) first = entry.next;
-            if (entry == last) last = entry.pre;
-        }
-        hashMap.remove(key);
-    }
-
-    private void moveToFirst(Entry entry) {
-        if (entry == first) return;
-        if (entry.pre != null) entry.pre.next = entry.next;
-        if (entry.next != null) entry.next.pre = entry.pre;
-        if (entry == last) last = last.pre;
-
-        if (first == null || last == null) {
-            first = last = entry;
-            return;
-        }
-        entry.next = first;
-        first.pre = entry;
-        first = entry;
-        entry.pre = null;
-    }
-    private void removeLast() {
-        if (last != null) {
-            last = last.pre;
-            if (last == null) first = null;
-            else last.next = null;
+        } else {
+            // 更新cache
+            node.value = value;
+            moveToFirst(node);
         }
     }
-
-    private Entry<K, V> getEntry(K key) {
-        return hashMap.get(key);
-    }
-
-    private HashMap<K, Entry<K, V>> hashMap;
-
-    class Entry<K, V> {
-        public Entry pre;
-        public Entry next;
-        public K key;
-        public V value;
+    class DlinkNode {
+        public DlinkNode pre;
+        public DlinkNode post;
+        public int key;
+        public String value;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        Entry entry = first;
-        while (entry != null) {
-            sb.append(String.format("%s:%s ", entry.key, entry.value));
-            entry = entry.next;
+        DlinkNode node = head;
+        while (node != null) {
+            sb.append(String.format("%s:%s ", node.key, node.value));
+            node = node.post;
         }
         return sb.toString();
     }
