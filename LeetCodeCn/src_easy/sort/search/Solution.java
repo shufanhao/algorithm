605fad67f89e25342e6d8d541fb653031f6c8791
fanhao <shufanhao@126.com>
2019-04-21 22:20:37 +0800

    update merge issue

--- LeetCodeCn/src_easy/sort/search/Solution.java
+++ LeetCodeCn/src_easy/sort/search/Solution.java
@@ -9,6 +9,7 @@ public class Solution {
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i,j,k;
+        // for 条件写的比较好
         for (i=m - 1,j=n -1,k=m+n-1; k>=0; k--) {
             if ((i>=0) && ( j<0 || nums1[i] > nums2[j])) {
                 nums1[k] = nums1[i--];
