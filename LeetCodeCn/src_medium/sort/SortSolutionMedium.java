47c509490ff8441887237bd7f4b3b8515d1f1925
Hao Fan <h_fan@apple.com>
2019-12-08 15:50:59 +0800

    题目4：寻找峰值

--- LeetCodeCn/src_medium/sort/SortSolutionMedium.java
+++ LeetCodeCn/src_medium/sort/SortSolutionMedium.java
@@ -42,8 +42,6 @@ class SortSolutionMedium {
 
     /**
      * 题目3：数组中第K个最大元素
-     * @param nums
-     * @param k
      */
     public int findKthLargest(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
@@ -54,6 +52,30 @@ class SortSolutionMedium {
         return nums[nums.length - k];
     }
 
+    /**
+     * 题目4：寻找峰值
+     * 思路：题目要求时间复杂度O(logN),要想到二分法。
+     * 注意题目条件：nums[i] ≠ nums[i+1]， 并且nums[-1] = nums[n] = -∞
+     * 如果nums[i] < nums[i+1]，则峰值一定出现在i的右侧，否则左侧
+     * refer: https://leetcode-cn.com/problems/find-peak-element/solution/hua-jie-suan-fa-162-xun-zhao-feng-zhi-by-guanpengc/
+     */
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String args[]) {
         SortSolutionMedium medium = new SortSolutionMedium();
         int [] nums2 = new int[]{1, 1, 1, 2, 2, 3};
@@ -61,5 +83,8 @@ class SortSolutionMedium {
 
         int [] nums3 = new int[]{3,2,1,5,6,4};
         System.out.println("题目3：" + medium.findKthLargest(nums3, 2));
+
+        int [] nums4 = new int[]{1,2,1,3,5,6,4};
+        System.out.println("题目4：" + medium.findPeakElement(nums4));
     }
 }
