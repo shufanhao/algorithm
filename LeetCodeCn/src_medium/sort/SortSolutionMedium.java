5717044a99a62294a92b131cc61949ad9c5a4c96
Hao Fan <h_fan@apple.com>
2019-12-08 10:58:13 +0800

    add: 前 K 个高频元素

--- LeetCodeCn/src_medium/sort/SortSolutionMedium.java
+++ LeetCodeCn/src_medium/sort/SortSolutionMedium.java
@@ -1,12 +1,17 @@
 package sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class SortSolutionMedium {
-    // Ref: https://blog.csdn.net/x273591655/article/details/84001236
+
+    /**
+     * 题目2：前 K 个高频元素
+     * Ref: https://blog.csdn.net/x273591655/article/details/84001236
+     */
     public List<Integer> topKFrequent(int[] nums, int k) {
         // 统计元素的频次
         Map<Integer, Integer> int2FreqMap = new HashMap<>(16);
@@ -33,5 +38,28 @@ class SortSolutionMedium {
         }
 
         return ret;
+    }
+
+    /**
+     * 题目3：数组中第K个最大元素
+     * @param nums
+     * @param k
+     */
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    public static void main(String args[]) {
+        SortSolutionMedium medium = new SortSolutionMedium();
+        int [] nums2 = new int[]{1, 1, 1, 2, 2, 3};
+        System.out.println("题目2：" + medium.topKFrequent(nums2, 2));
+
+        int [] nums3 = new int[]{3,2,1,5,6,4};
+        System.out.println("题目3：" + medium.findKthLargest(nums3, 2));
     }
 }
