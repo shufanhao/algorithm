6de05cc271151b331af00f9656271d31a66c0b85
shufanhao <shufanhao@126.com>
2023-04-02 11:12:49 +0800

    add: TrieNode

---
+++ src/main/java/com/haofan/algorithm/offer/trie/TrieSolution.java
@@ -0,0 +1,42 @@
+package com.haofan.algorithm.offer.trie;
+
+
+public class TrieSolution {
+    private TrieNode root = new TrieNode();
+
+    public void insert(String world) {
+        TrieNode node = root;
+        for (char ch : world.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new TrieNode();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.isWorld = true;
+    }
+
+    public boolean search(String world) {
+        TrieNode node = root;
+
+        for (char ch : world.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+        return node.isWorld;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+
+        for (char ch : prefix.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+
+        return true;
+    }
+}
