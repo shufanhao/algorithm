92c07a5abdfc1b2b14d8a63e01c5f221bc431134
shufanhao <shufanhao@126.com>
2024-03-10 17:18:37 +0800

    update: queue solution

--- src/test/java/com/haofan/algorithm/queue/QueueSolutionTest.java
+++ src/test/java/com/haofan/algorithm/queue/QueueSolutionTest.java
@@ -4,6 +4,9 @@ import com.haofan.algorithm.help.TreeNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 class QueueSolutionTest {
 
     private QueueSolution queue = new QueueSolution();
@@ -14,6 +17,30 @@ class QueueSolutionTest {
         movingAverage.next(1);
         movingAverage.next(2);
         Assertions.assertEquals(2, movingAverage.next(3));
+
+        System.out.println(getMaxTeamNaive(new Integer[]{6, 6, 4, 3, 2}));
+
+
+    }
+
+        // Solution 1: 每次选择最top 3 的三个team，然后再排序。
+        // Time Complexity: O(m * nlogn). Here m is the max numbers of teams and n is the no.of states.
+        // Additional Space Complexity: O(1)
+    int getMaxTeamNaive(Integer[] players) {
+        if (players == null || players.length < 3) {
+            return 0;
+        }
+
+        int count = 0;
+        Arrays.sort(players, Collections.reverseOrder());
+        while (players[0] > 0 && players[1] > 0 && players[2] > 0) {
+            count ++;
+            players[0] -= 1;
+            players[1] -= 1;
+            players[2] -= 1;
+            Arrays.sort(players, Collections.reverseOrder());
+        }
+        return count;
     }
 
     @Test
